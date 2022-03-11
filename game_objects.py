1a7f54ee37aa0bc4b1a748c628fddcfca85bb44b
Sahachat Hukhiao <autogran41@gmail.com>
2022-03-11 16:15:06 +0700

    Saperate game entitys from main file game.

---
+++ game_objects.py
@@ -0,0 +1,138 @@
+from kivy.graphics import Rectangle
+
+class Entity(object):
+    def __init__(self):
+        self._pos = (0, 0)
+        self._size = (0, 0)
+        self._source = "image/playerfr1.png"
+        self._instruction = Rectangle(
+            pos=self._pos, size=self._size, source=self._source)
+
+    @property
+    def pos(self):
+        return self._pos
+
+    @pos.setter
+    def pos(self, value):
+        self._pos = value
+        self._instruction.pos = self._pos
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self._size = value
+        self._instruction.size = self._size
+
+    @property
+    def source(self):
+        return self._source
+
+    @source.setter
+    def source(self, value):
+        self._source = value
+        self._instruction.source = self._source
+
+
+class Player(Entity):
+    def __init__(self,game, **kwargs):
+        super().__init__(**kwargs)
+        self.game = game
+        self.playerState = 0
+        self.size = (150,160)
+        # self.source = "image/player.png"
+        self.game.bind(on_frame=self.move_step)
+        self.pos = (400, 0)
+
+
+    def stop_callbacks(self):
+        self.game.unbind(on_frame=self.move_step)
+
+    # Move
+    def move_step(self, sender, dt):
+        currentpic = self.source
+        currentx = self.pos[0]
+        currenty = self.pos[1]
+
+        step_size = 500 * (dt)
+
+        if "w" in self.game.keysPressed:
+            if self.playerState < 7:
+                currentpic = F"image/playerfr{self.playerState + 1}.png"
+                print("w")
+                self.playerState += 1
+            else:
+                self.playerState = 0
+                currentpic = F"image/playerfr{self.playerState + 1}.png"
+            currenty += step_size
+        if "s" in self.game.keysPressed:
+            if self.playerState < 7:
+                currentpic = F"image/playerfr{self.playerState + 1}.png"
+                print("s")
+                self.playerState += 1
+            else:
+                self.playerState = 0
+                currentpic = F"image/playerfr{self.playerState + 1}.png"
+            currenty -= step_size
+        if "a" in self.game.keysPressed:
+            if self.playerState < 7:
+                currentpic = F"image/playerfr{self.playerState + 1}.png"
+                print("a")
+                self.playerState += 1
+            else:
+                self.playerState = 0
+                currentpic = F"image/playerfr{self.playerState + 1}.png"
+            currentx -= step_size
+        if "d" in self.game.keysPressed:
+            if self.playerState < 7:
+                currentpic = F"image/playerfr{self.playerState + 1}.png"
+                print("d")
+                self.playerState += 1
+            else:
+                self.playerState = 0
+                currentpic = F"image/playerfr{self.playerState + 1}.png"
+            currentx += step_size
+
+        self.pos = (currentx, currenty)
+        self.source = currentpic
+
+
+class Items(Entity):
+    def __init__(self, pos, speed, item_type, game):
+        super().__init__()
+        self.game = game
+        self._speed = speed
+        self.item_type = item_type
+        self.size = (50, 50)
+        self.pos = pos
+        self.source = F"image/asset/{item_type}.jpg"
+        self.game.bind(on_frame=self.move_step)
+
+    def stop_callbacks(self):
+        self.game.unbind(on_frame=self.move_step)
+
+    def move_step(self, sender, dt):
+        for e in self.game.colliding_entities(self):
+            if e == self.game.player:
+                self.stop_callbacks()
+                self.game.remove_entity(self)
+                print(F"collide! {self.item_type}")
+                if self.item_type == "Bomb":
+                    self.game.half_score()
+                    print("BOMB")
+                    is_alpha = False
+                elif self.item_type == "Eraser":
+                    self.game.clear_items()
+                    is_alpha = False
+                else:
+                    self.game.add_items(self.item_type)
+                    is_alpha = True
+                self.game.refresh_word(is_alpha)
+                print(self.game.get_items)
+                return
+        step_size = self._speed * dt
+        new_x = self.pos[0]
+        new_y = self.pos[1] - step_size
+        self.pos = (new_x, new_y)         
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from tkinter import Button
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
 from kivy.core.window import Window
 from kivy.clock import Clock
-from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
-)
 from kivy.core.window import Window
 from kivy.core.audio import SoundLoader
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.uix.label import CoreLabel
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -18,6 +11,8 @@ from kivy.config import Config
 
 import random
 import pandas as pd
+
+from game_objects import Player,Items
 
 Config.set('graphics', 'maxfps', '60')
 
@@ -45,6 +40,9 @@ class GameMain(Screen):
         self.sound.loop = True
         self.sound.play()
 
+    def test(self,level):
+        print(level)
+
     def initial(self):
         self._keyboard = Window.request_keyboard(
             self._on_keyboard_closed, self)
@@ -52,6 +50,10 @@ class GameMain(Screen):
         self._keyboard.bind(on_key_up=self._on_key_up)
 
         self._df = pd.read_csv('words.csv')
+        self.w = self._df['Word'].astype('str')
+        m = (self.w.str.len() >= 8) & (self.w.str.len() < 10)
+        print(self._df[m])
+        print(len(self._df[m]))
         rand_word = random.randint(1, len(self._df['Word']))
         self.word_rand = self._df.iloc[rand_word]['Word']
         self.def_word = self._df.iloc[rand_word]['Definition']
@@ -223,152 +225,13 @@ class GameMain(Screen):
         self._score_label.refresh()
         self._score_instruction.texture = self._score_label.texture
         self._score_instruction.size = self._score_label.texture.size
-        
-
-
-class Entity(object):
-    def __init__(self):
-        self._pos = (0, 0)
-        self._size = (0, 0)
-        self._source = "image/playerfr1.png"
-        self._instruction = Rectangle(
-            pos=self._pos, size=self._size, source=self._source)
-
-    @property
-    def pos(self):
-        return self._pos
-
-    @pos.setter
-    def pos(self, value):
-        self._pos = value
-        self._instruction.pos = self._pos
-
-    @property
-    def size(self):
-        return self._size
-
-    @size.setter
-    def size(self, value):
-        self._size = value
-        self._instruction.size = self._size
-
-    @property
-    def source(self):
-        return self._source
-
-    @source.setter
-    def source(self, value):
-        self._source = value
-        self._instruction.source = self._source
-
-
-class Player(Entity):
-    def __init__(self,game, **kwargs):
-        super().__init__(**kwargs)
-        self.game = game
-        self.playerState = 0
-        self.size = (150,160)
-        # self.source = "image/player.png"
-        self.game.bind(on_frame=self.move_step)
-        self.pos = (400, 0)
-
-
-    def stop_callbacks(self):
-        self.game.unbind(on_frame=self.move_step)
-
-    # Move
-    def move_step(self, sender, dt):
-        currentpic = self.source
-        currentx = self.pos[0]
-        currenty = self.pos[1]
-
-        step_size = 500 * (dt)
-
-        if "w" in self.game.keysPressed:
-            if self.playerState < 7:
-                currentpic = F"image/playerfr{self.playerState + 1}.png"
-                print("w")
-                self.playerState += 1
-            else:
-                self.playerState = 0
-                currentpic = F"image/playerfr{self.playerState + 1}.png"
-            currenty += step_size
-        if "s" in self.game.keysPressed:
-            if self.playerState < 7:
-                currentpic = F"image/playerfr{self.playerState + 1}.png"
-                print("s")
-                self.playerState += 1
-            else:
-                self.playerState = 0
-                currentpic = F"image/playerfr{self.playerState + 1}.png"
-            currenty -= step_size
-        if "a" in self.game.keysPressed:
-            if self.playerState < 7:
-                currentpic = F"image/playerfr{self.playerState + 1}.png"
-                print("a")
-                self.playerState += 1
-            else:
-                self.playerState = 0
-                currentpic = F"image/playerfr{self.playerState + 1}.png"
-            currentx -= step_size
-        if "d" in self.game.keysPressed:
-            if self.playerState < 7:
-                currentpic = F"image/playerfr{self.playerState + 1}.png"
-                print("d")
-                self.playerState += 1
-            else:
-                self.playerState = 0
-                currentpic = F"image/playerfr{self.playerState + 1}.png"
-            currentx += step_size
-
-        self.pos = (currentx, currenty)
-        self.source = currentpic
-
-
-class Items(Entity):
-    def __init__(self, pos, speed, item_type, game):
-        super().__init__()
-        self.game = game
-        self._speed = speed
-        self.item_type = item_type
-        self.size = (50, 50)
-        self.pos = pos
-        self.source = F"image/asset/{item_type}.jpg"
-        self.game.bind(on_frame=self.move_step)
-
-    def stop_callbacks(self):
-        self.game.unbind(on_frame=self.move_step)
-
-    def move_step(self, sender, dt):
-        for e in self.game.colliding_entities(self):
-            if e == self.game.player:
-                self.stop_callbacks()
-                self.game.remove_entity(self)
-                print(F"collide! {self.item_type}")
-                if self.item_type == "Bomb":
-                    self.game.half_score()
-                    print("BOMB")
-                    is_alpha = False
-                elif self.item_type == "Eraser":
-                    self.game.clear_items()
-                    is_alpha = False
-                else:
-                    self.game.add_items(self.item_type)
-                    is_alpha = True
-                self.game.refresh_word(is_alpha)
-                print(self.game.get_items)
-                return
-        step_size = self._speed * dt
-        new_x = self.pos[0]
-        new_y = self.pos[1] - step_size
-        self.pos = (new_x, new_y)                                                                                                   
                                                                                                    
-                                                                                                                                                                                                    
-
-
-# enemy = Items((500,500))
-# game.add_entity(enemy)
+                                                                                                   
+        
 class GameLevelMenuScreen(Screen):
+    def pressBtn(self, level):
+        # print(level)
+        GameMain().test(level)
     pass
 
 class MenuScreen(Screen):
@@ -382,6 +245,5 @@ class MyApp(App):
     def build(self):
         return kv
 
-   
 if __name__ == "__main__":
     MyApp().run()
