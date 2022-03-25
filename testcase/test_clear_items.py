e478d5542d4df71a5e9cc82f64825e46fc2c0deb
Poramee Madadam <poramee0.50@gmail.com>
2022-03-25 22:13:59 +0700

    add test_clear_item_CHEM

--- testcase/test_clear_items.py
+++ testcase/test_clear_items.py
@@ -1,3 +1,4 @@
+from unittest import result
 from main import GameMain
 import unittest
 
@@ -5,9 +6,25 @@ class ClearItemTest(unittest.TestCase):
     def test_clear_item_B(self):
         game = GameMain()
         game.add_items("B")
+        
         game.clear_items()
 
         expected_result = ""
 
         result = game.get_items
         self.assertEqual(result, expected_result)
+    
+    def test_clear_item_CHEM(self):
+        game = GameMain()
+        list_name = ['C','H','E','M']
+        for i in list_name:
+            game.add_items(i)
+        
+        game.clear_items()
+
+        expected_result = ""
+
+        result = game.get_items
+        self.assertEqual(result, expected_result)
+    
+    
