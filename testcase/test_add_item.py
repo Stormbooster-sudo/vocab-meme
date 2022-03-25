89e8efb1071029ed1cbe733985e75f2114ff3d98
กฤตนัย โกยพิพัฒน์กุล <kittanai545@gmail.com>
2022-03-25 22:09:08 +0700

    test_add_PANG

--- testcase/test_add_item.py
+++ testcase/test_add_item.py
@@ -13,22 +13,23 @@ class AddItemTest(unittest.TestCase):
 
     def test_add_CHADARAT(self):
         game = GameMain()
-        list_name = ['C','H','A','D','A','R','A','T']
+        list_name = ["C","H","A","D","A","R","A","T"]
         for i in list_name:
             game.add_items(i)
 
-        expected_result = 'CHADARAT'
+        expected_result = "CHADARAT"
 
         result = game.get_items
         self.assertEqual(result, expected_result)
 
-    def test_add_CHADARAT(self):
+    def test_add_PANG(self):
         game = GameMain()
-        list_name = ['C','H','A','D','A','R','A','T']
+        list_name = ["P","A","N","G"]
         for i in list_name:
             game.add_items(i)
 
-        expected_result = 'CHADARAT'
+        expected_result = "PANG"
 
         result = game.get_items
         self.assertEqual(result, expected_result)
+
