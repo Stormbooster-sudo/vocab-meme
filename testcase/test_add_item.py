2f9c4bb85cada6549006c4ea045767d6b32cb420
กฤตนัย โกยพิพัฒน์กุล <kittanai545@gmail.com>
2022-03-25 22:02:19 +0700

    test_add_CHADARAT

--- testcase/test_add_item.py
+++ testcase/test_add_item.py
@@ -11,4 +11,24 @@ class AddItemTest(unittest.TestCase):
         result = game.get_items
         self.assertEqual(result, expected_result)
 
-    
+    def test_add_CHADARAT(self):
+        game = GameMain()
+        list_name = ['C','H','A','D','A','R','A','T']
+        for i in list_name:
+            game.add_items(i)
+
+        expected_result = 'CHADARAT'
+
+        result = game.get_items
+        self.assertEqual(result, expected_result)
+
+    def test_add_CHADARAT(self):
+        game = GameMain()
+        list_name = ['C','H','A','D','A','R','A','T']
+        for i in list_name:
+            game.add_items(i)
+
+        expected_result = 'CHADARAT'
+
+        result = game.get_items
+        self.assertEqual(result, expected_result)
